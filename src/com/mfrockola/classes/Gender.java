b2ac82709ed957b63487c67bbe772de1e1cc736b
runegame <alm93@hotmail.com>
2017-02-01 14:24:47 -0400

    New changes for English language

--- src/com/mfrockola/classes/Gender.java
+++ src/com/mfrockola/classes/Gender.java
@@ -3,7 +3,7 @@ package com.mfrockola.classes;
 /**
  * This class is the definition of a musical genre. Contains its name and the list of songs in a Song arrangement
  */
-public class Gender {
+class Gender {
 
     // private variable to store the gender name.
     private String name;
@@ -12,25 +12,25 @@ public class Gender {
     private Song[] songs;
 
     // in the constructor we establish the private variables
-    public Gender(String name, Song[] songs) {
+    Gender(String name, Song[] songs) {
         setName(name);
         setSongs(songs);
     }
 
     // Public methods to obtain the values ​​of the private variables of the Gender class
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public void setName(String nombre) {
+    private void setName(String nombre) {
         this.name = nombre;
     }
 
-    public Song[] getSongs() {
+    Song[] getSongs() {
         return songs;
     }
 
-    public void setSongs(Song[] songs) {
+    private void setSongs(Song[] songs) {
         this.songs = songs;
     }
 }
